4b33ef8a73af4fcb10feb061ba0b2e2a22d15f8b
Chris Kubick <socalledsound@gmail.com>
2021-05-20 13:29:48 -0700

    update route url

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/scores', scoreRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/Scores.js
+++ routes/Scores.js
@@ -12,5 +12,28 @@ scoreRouter.route('/')
             res.json(scores);
         })
     })
+    .post((req, res, next) => {
+        Scores.create(req.body)
+        .then(score => {
+            console.log('score added: ', score)
+            res.statusCode  = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.json(score)
+        })
+        .catch(err => next(err))
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /scores');
+    })
+    .delete((req, res, next) => {
+        Scores.deleteMany()
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err));
+    });
   
 module.exports = scoreRouter;
